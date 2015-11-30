de9b3b899e7b1bec006ea8851a54cb8fa06eeab5
Rafa Alves <raffa-alves@hotmail.com.br>
2015-11-30 03:28:34 -0200

    globalization

--- platforms/ios/www/js/my-app.js
+++ platforms/ios/www/js/my-app.js
@@ -382,3 +382,13 @@ function openPage(url) {
 function ucFirst(string) {
     return string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
 }
+function loadJS(file) {
+    // DOM: Create the script element
+    var jsElm = document.createElement("script");
+    // set the type attribute
+    jsElm.type = "application/javascript";
+    // make the script element load file
+    jsElm.src = file;
+    // finally insert the element to the body element in order to load the script
+    document.body.appendChild(jsElm);
+}
