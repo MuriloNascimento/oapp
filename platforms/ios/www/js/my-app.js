9d3a4d4a0a3d66c239e6fdf5bddb160d79f80718
Rafa Alves <raffa-alves@hotmail.com.br>
2016-01-02 03:48:04 -0200

    Connection

--- platforms/ios/www/js/my-app.js
+++ platforms/ios/www/js/my-app.js
@@ -16,7 +16,7 @@ var myApp = new Framework7({
 //var host = 'http://app.orvipclub.com'
 var host = 'http://checklist.grupoair.com.br'
 
-myApp.onPageInit('login', function (page) {
+myApp.onPageAfterInit('login', function (page) {
     alert('qq');
     //checkLanguage();
 }); 
