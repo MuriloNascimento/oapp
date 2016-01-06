6124ed40c53d393a4b5b8970d6c7a6f9b230dfa4
Murilo Nascimento <murilo.nascimento1988@gmail.com>
2016-01-06 15:43:21 -0200

    teste

--- platforms/ios/www/js/my-app.js
+++ platforms/ios/www/js/my-app.js
@@ -639,35 +639,29 @@ var lang_text = 'en';
 function checkLanguage() {
 
     /*=====DEV=====*/
-    //lang_text = 'pt';
-    //lang = window['pt'];
-    //changeText();
+    lang_text = 'en';
+    lang = window['en'];
+    changeText();
     
     /*=====PROD=====*/
-    navigator.globalization.getPreferredLanguage(
-        function (language) {
-            var l_ = language.value;
-            var lang_ = l_.split("-");
-
-            //lang_text = lang_[0];
-            //console.log(lang_);
-            //console.log(lang_text);
-            //lang = window[lang_text];
-            //changeText();
-
-            lang_text = 'pt';
-            lang = window['pt'];
-            changeText();
-        },
-        function () {
-            lang = window[lang_text];
-            changeText();
-        }
-    );
+    //navigator.globalization.getPreferredLanguage(
+    //    function (language) {
+    //        var l_ = language.value;
+    //        var lang_ = l_.split("-");
+    //
+    //        lang_text = lang_[0];
+    //        lang = window[lang_text];
+    //        changeText();
+    //    },
+    //    function () {
+    //        lang = window[lang_text];
+    //        changeText();
+    //    }
+    //);
 }
 function changeText(){
     $.each(lang, function(i, val) {
-        $('.lang_-'+i).text(val);
+       $('.lang_-'+i).text(val);
     });
 }
 
